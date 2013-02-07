ff9f8cf6ee8ae2e5cfca95193442c3b8175a6802
Tatsuya Sawai <a209050@ns.kogakuin.ac.jp>
2013-02-08 00:19:01 +0900

    First commit

---
+++ SetKogakuinProxyApp/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace SetSystemProxyApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(154, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 63);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "On";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "八王子",
+            "新宿"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 22);
+            this.label1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(276, 81);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "SetKogakuinProxyApp";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SetKogakuinProxyApp/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace SetSystemProxyApp
+{
+    public partial class Form1 : Form
+    {
+
+        private static string regKeyName = @"Software\Microsoft\Windows\CurrentVersion\Internet Settings";
+        private static string hachiouji = "cache.ns.kogakuin.ac.jp:8080";
+        private static string shinzyuku = "cache.ccs.kogakuin.ac.jp:8080";
+        private RegistryKey rKey = Registry.CurrentUser.OpenSubKey(regKeyName, true);
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            object defVal = rKey.GetValue("ProxyEnable");
+            if (double.Parse(defVal.ToString()) == 1)
+            {
+                button1.Text = "Off";
+                label1.BackColor = Color.Lime;
+            }
+            else
+            {
+                button1.Text = "On";
+                label1.BackColor = Color.Red;
+            }
+
+            object defVal2 = rKey.GetValue("ProxyServer");
+            if (defVal2.ToString() == hachiouji)
+            {
+                //comboBox1.SelectedIndex == 1; SelectedItem == "八王子";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string setValue = "";
+            int enabled;
+
+            if (comboBox1.SelectedItem == null)
+            {
+                setValue = "";
+            }
+            else if (comboBox1.SelectedItem.Equals("八王子"))
+            {
+                setValue = hachiouji;
+            }
+            else if (comboBox1.SelectedItem.Equals("新宿"))
+            {
+                setValue = shinzyuku;
+            }
+
+            if (button1.Text == "On")
+            {
+                enabled = 1;
+                button1.Text = "Off";
+                label1.BackColor = Color.Lime;
+            }
+            else
+            {
+                enabled = 0;
+                button1.Text = "On";
+                label1.BackColor = Color.Red;
+            }
+
+            // レジストリの設定と削除
+            try
+            {
+                RegistryKey rKey = Registry.CurrentUser.OpenSubKey(regKeyName, true);
+                rKey.SetValue("ProxyServer", setValue, RegistryValueKind.String);
+                rKey.SetValue("ProxyEnable", enabled, RegistryValueKind.DWord);
+                rKey.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error Occured." + ex.ToString());
+            }
+        }
+    }
+}
